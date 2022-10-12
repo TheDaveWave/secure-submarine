394a21d4d7363244f3d262046a92813bf5c5800d
David Lindberg <david.wesley.lindberg@gmail.com>
2022-10-12 13:28:57 -0500

    added rejectUnauthenticated to secrets.router.js

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
 
